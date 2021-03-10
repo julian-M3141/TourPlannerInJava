30161d4c2a78f1124978f1016ab1673f839ca7ff
Julian Mucherl <if19b161@technikum-wien.at>
2021-03-10 11:02:49 +0100

    first window with input and button

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Tour Planner");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 500, 275));
         primaryStage.show();
     }
 
