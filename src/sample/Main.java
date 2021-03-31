ffd7a4e7675cec132b14e735c99dd3d4b4dbfbee
Julian Mucherl <if19b161@technikum-wien.at>
2021-03-31 15:12:45 +0200

    added tourreportpdf

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main extends Application {
 
     @Override
@@ -20,6 +23,15 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        TourReport report = new TourReport();
+        try {
+            report.print(new Tour("Lange tour","200km", "London" , "Peking" , "long description ... ", new ArrayList<Log>(){{
+                add(new Log("12.1.2021", "12h", "5 stars"));
+                add(new Log("18.1.2021", "11h", "4.5 stars"));
+            }}));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         launch(args);
     }
 }
---
+++ src/sample/TourReport.java
@@ -0,0 +1,80 @@
+package sample;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentInformation;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class TourReport {
+    public void print(Tour tour) throws IOException {
+        PDDocument document = new PDDocument();
+        PDDocumentInformation info = document.getDocumentInformation();
+        //set information
+        info.setAuthor("TourPlanner");
+        info.setTitle(tour.getTourname());
+        info.setCreator("TourPlanner");
+        info.setSubject("Tour "+tour.getTourname());
+        info.setCreationDate(Calendar.getInstance());
+        info.setModificationDate(Calendar.getInstance());
+
+        //create new page
+        PDPage page = new PDPage();
+        document.addPage(page);
+
+        //create new stream
+        PDPageContentStream stream = new PDPageContentStream(document,page);
+
+        //write to stream
+        stream.beginText();
+
+        //set font size
+        stream.setFont(PDType1Font.TIMES_ROMAN,24);
+        //set position
+        stream.newLineAtOffset(25,750);
+        //write to stream
+        stream.showText(tour.getTourname());
+
+
+        stream.endText();
+
+        //draw image
+        //later tour.getImage
+        PDImageXObject image = PDImageXObject.createFromFile("test.jpg",document);
+        stream.drawImage(image,25,400,450,300);
+
+        stream.beginText();
+        stream.newLineAtOffset(25,350);
+        stream.setFont(PDType1Font.TIMES_ROMAN,12);
+        stream.setLeading(14.5f);
+        stream.showText(tour.getDescription());
+
+        stream.newLine();
+        stream.showText("Logs:");
+        stream.newLine();
+        ArrayList<Log> logs = tour.getLogs();
+        for(int i=0;i<logs.size(); ++i){
+            stream.showText("Log "+(i+1));
+            stream.newLine();
+            stream.showText("Datum: "+logs.get(i).getDate());
+            stream.newLine();
+            stream.showText("Zeit: "+logs.get(i).getTime());
+            stream.newLine();
+            stream.showText("Rating: "+logs.get(i).getRating());
+            stream.newLine();
+        }
+
+        stream.endText();
+        stream.close();
+
+        document.save("test.pdf");
+        document.close();
+
+    }
+}
